3563b4ae651b0f6e0d16df2b40755539257b79a2
KakashiJiyayou <rafibapi@gmail.com>
2023-07-18 09:22:05 -0700

    Optional APi

---
+++ counter_with_optional_api/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <main>
+    <div class= "container">
+      <h4> Counter </h4>
+      <h1> {{ counter }} </h1>
+      <button @click= "substraction" >-</button>
+      <button @click= "increament">+</button>
+    </div >
+  </main>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    increament () {
+      this.counter = 1 + this.counter
+    },
+
+    substraction () {
+      this.counter = this.counter - 1
+    }
+  }
+}
+</script>
